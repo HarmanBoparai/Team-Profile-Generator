ffefe3cc506a58a06cdfdad4f67f847708276971
HarmanBoparai <erharmanboparai@gmail.com>
2022-10-13 22:15:17 -0400

    Added Demo video

---
+++ generateHTML.js
@@ -0,0 +1,15 @@
+
+
+// A function to generate markdown for README
+function generateHTML({
+  
+}) {
+
+ # [github username](https://github.com/${github_name})
+ # ${email}
+
+
+`;
+}
+
+module.exports = generateHTML;
