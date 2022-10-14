72de571edcf1c38294c60e2ab0347697361429c9
HarmanBoparai <erharmanboparai@gmail.com>
2022-10-13 21:47:46 -0400

    Created questions in index.js file

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
 // Packages for this application 
 const inquirer=require('inquirer');
 const fs=require('fs');
+const generateHTML=require('./generateHTML');
 
-// Different role files
-const Engineer = require('./lib/engineer');
-const Manager = require('./lib/manager');
-const Intern = require('./lib/intern');
+const generateHTMLfile=() =>{
+    inquirer
+    .prompt([
+        const generateHTMLfile=() =>{
+            inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'team member info',
+                message: 'Please add team member information?',
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Do you want to include your email?',
+              },
+              {
+                type: 'input',
+                name: 'github_name',
+                message: 'Enter your github username?',
+              },
+              {
+                type: 'input',
+                 name: 'Manager info',
+                message: 'Please add manager information?',
+              },
+              {
+                type: 'list',
+                name: 'Team Building',
+                message: 'Do you like to build a team?',
+                choices: ["Engineer","Intern"]
+              },
+              {
+                type: 'input',
+                name: 'Engineer Info',
+                message: 'Please add an engineer info?',
+              },
+              {
+                type: 'input',
+                name: 'Intern Info',
+                message:'Please add an Intern info?',
+              },
+        {
+            type: 'list',
+            name: 'TeamMember',
+            message: 'Would do you like another team member?',
+            choices: ["Yes","No"]
+          },
+        ]).then((useranswers) =>{
+            console.log(useranswers)
+          //   const readmePageContent =generateHTMLFile(useranswers);
+          const readmePageContent=generateHTML(useranswers)
+           //console.log(generateMarkdown(useranswers))
+        
+           //writefile fuction to write the answers given by the user to the prompt to the README file
+           fs.writeFile('./Dist/Output/Index.html',readmePageContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created Index.html!')
+          );
+          });
+        }
+        
